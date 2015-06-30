dd61a3e79050a672984f5ffd9ccba14b9e3c458b
Timothy Nyachio <sarrtim@gmail.com>
2015-06-30 17:11:45 +0300

    Initial commit

---
+++ Controllers/PharmacyController.cs
@@ -0,0 +1,37 @@
+﻿using EPRES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPRES.Controllers
+{
+    [Authorize]
+    public class PharmacyController : Controller
+    {
+        // GET: Pharmacy
+        public ActionResult Pharmacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Pharmacy(PharmacyMaster U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (EpresDbEntities dc = new EpresDbEntities())
+                {
+                    dc.PharmacyMasters.Add(U);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Successfully saved pharmacy detail";
+                }
+            }
+            return View(U);
+        }
+    }
+}
---
+++ Controllers/PrescriptionController.cs
@@ -0,0 +1,36 @@
+﻿using EPRES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPRES.Controllers
+{
+    [Authorize]
+    public class PrescriptionController : Controller
+    {
+        // GET: Prescription
+        public ActionResult Prescription()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Prescription(PrescriptionMaster P)
+        {
+            if (ModelState.IsValid)
+            {
+                using (EpresDbEntities dc = new EpresDbEntities())
+                {
+                    dc.PrescriptionMasters.Add(P);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    P = null;
+                    ViewBag.Message = "Successfully saved prescription detail";
+                }
+            }
+            return View(P);
+        }
+    }
+}
